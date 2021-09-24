f65d4c0f2c9a317bb7e65d3eb318143f574006b8
Samuel Martey <sam@encodevlabs.com>
2021-09-24 19:30:33 +0000

    added tailwind

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,7 +1,34 @@
 import React from "react";
+import "tailwindcss/tailwind.css";
 
 const App = () => {
-	return <div>Hello from somewhere</div>;
+	return (
+		<div className='flex justify-center '>
+			<figure className="md:flex bg-gray-100 rounded-xl p-8 md:p-0">
+				<img
+					className="w-32 h-32 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto"
+					src="https://pbs.twimg.com/profile_images/1424905908920983552/uh-sO2Y__400x400.jpg"
+					alt=""
+					width="384"
+					height="512"
+				/>
+				<div className="pt-6 md:p-8 text-center md:text-left space-y-4">
+					<blockquote>
+						<p className="text-lg font-semibold">
+							“Tailwind CSS is the only framework that I've seen scale on large
+							teams. It’s easy to customize, adapts to any design, and the build
+							size is tiny.”
+						</p>
+					</blockquote>
+					<figcaption className="font-medium">
+						<div className="text-cyan-600">Sarah Dayan</div>
+						<div className="text-gray-500">Staff Engineer, Algolia</div>
+					</figcaption>
+				</div>
+			</figure>
+
+	</div>
+	);
 };
 
 export default App;
